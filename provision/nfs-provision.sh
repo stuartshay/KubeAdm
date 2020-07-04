cbbb6f0f0746590e7a4a6a1b2033ad8c6f13b025
xenon-support <akira@xenonstack.com>
2020-07-04 11:35:14 +0530

    updated storage class as default

--- provision/nfs-provision.sh
+++ provision/nfs-provision.sh
@@ -16,6 +16,9 @@ mkdir -p /srv/nfs/kubetesting
 mkdir -p /srv/nfs/kubetesting/bitnami-wordpress
 mkdir -p /srv/nfs/kubetesting/storage
 mkdir -p /srv/nfs/kubetesting/storageclass-data
+mkdir -p /srv/nfs/kubetesting/storageclass-data2
+mkdir -p /srv/nfs/kubetesting/storageclass-data3
+
 
 echo "[TASK 3] Update the shared folder access"
 sudo chown -R nobody:nogroup /srv/nfs/kubedata
@@ -42,6 +45,12 @@ sudo chmod 777 /srv/nfs/kubetesting/storage
 sudo chown -R nobody:nogroup /srv/nfs/kubetesting/storageclass-data
 sudo chmod 777 /srv/nfs/kubetesting/storageclass-data
 
+sudo chown -R nobody:nogroup /srv/nfs/kubetesting/storageclass-data2
+sudo chmod 777 /srv/nfs/kubetesting/storageclass-data2
+
+sudo chown -R nobody:nogroup /srv/nfs/kubetesting/storageclass-data3
+sudo chmod 777 /srv/nfs/kubetesting/storageclass-data3
+
 
 echo "[TASK 4 Deleting and re creating exports file for updation"
 sudo rm -r /etc/exports
@@ -56,6 +65,8 @@ cat >>/etc/exports<<EOF
 /srv/nfs/kubetesting/bitnami-wordpress    *(rw,sync,no_subtree_check,no_root_squash)
 /srv/nfs/kubetesting/storage   *(rw,sync,no_subtree_check,no_root_squash)
 /srv/nfs/kubetesting/storageclass-data   *(rw,sync,no_subtree_check,no_root_squash)
+/srv/nfs/kubetesting/storageclass-data2   *(rw,sync,no_subtree_check,no_root_squash)
+/srv/nfs/kubetesting/storageclass-data3   *(rw,sync,no_subtree_check,no_root_squash)
 EOF
 
 echo "[TASK 6] Export the updates"
